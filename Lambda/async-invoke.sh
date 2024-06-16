efb36d4ac6641eba95fb4fb75342e87cbe7d8f5d
Anthony Appau <anthony_appau@abtassoc.com>
2024-06-15 23:51:12 -0400

    clean up

--- Lambda/async-invoke.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FUNCTION_NAME="demo-lambda"
-
-aws lambda invoke --function-name $FUNCTION_NAME \
-    --cli-binary-format raw-in-base64-out \
-    --payload '{"key1": "Value1", "key2": "Value2", "key3": "Value3"}' \
-    --invocation-type Event \
-    --region us-east-1 
